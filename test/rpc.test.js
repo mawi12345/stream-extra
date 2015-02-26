e63136eb734fc5f95a0814998268cdef1b820b2c
Martin Wind <mawi1988@gmail.com>
2015-02-26 11:52:54 +0100

    add rpc progress notification

--- test/rpc.test.js
+++ test/rpc.test.js
@@ -15,6 +15,27 @@ var handler = {
 		var deferred = Q.defer();
 		deferred.resolve('hello '+params.name);
 		return deferred.promise;
+	},
+	
+	mkprogress: function(params) {
+		var deferred = Q.defer();
+		params = params || {};
+		var count = params.count || 10;
+		var timeout = params.timeout || 500;
+		
+		var i = 0;
+		var doTask = function() {
+			deferred.notify('task '+i+' done');
+			i++
+			if (i < count) {
+				setTimeout(doTask, timeout);
+			} else {
+				deferred.resolve('all tasks done');
+			}
+		}
+		setTimeout(doTask, timeout);
+		
+		return deferred.promise;
 	}
 }
 
@@ -25,23 +46,35 @@ var remoteClient = createStack(endpoints[1], handler);
 
 remoteServer.call('hello', {name: 'server'})
 .then(function(data){
-	console.log('client received '+data);
+	console.log('client received server.hello() result: '+data);
 }).fail(function(error){
-	console.error('client received '+error.message);
+	console.error('client received server.hello() error: '+error.message);
 });
 
 
 remoteServer.call('hello2', {name: 'lol'})
 .then(function(data){
-	console.log('client received '+data);
+	console.log('client received server.hello2() result: '+data);
 }).fail(function(error){
-	console.error('client received '+error.message);
+	console.error('client received server.hello2() error: '+error.message);
 });
 
 
 remoteClient.call('hello', {name: 'client'})
 .then(function(data){
-	console.log('server received '+data);
+	console.log('server received clinet.hello() result: '+data);
 }).fail(function(error){
-	console.error('remoteClient received '+error.message);
+	console.error('remoteClient received clinet.hello() error: '+error.message);
+});
+
+remoteClient.call('mkprogress', {count: 8})
+.then(function (result) {
+    // Success 
+	console.log('server received clinet.mkprogress() result: '+result);
+}, function (err) {
+    // There was an error
+	console.log('server received clinet.mkprogress() error: '+err.message);
+}, function (progress) {
+    // We get notified of the progress as it is executed
+	console.log('server received clinet.mkprogress() progress: '+progress);
 });
