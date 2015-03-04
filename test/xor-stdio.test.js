54773cf7fbc4ed41a53ed88add8f15a6d815ced8
Martin Wind <mawi1988@gmail.com>
2015-03-04 22:54:55 +0100

    fix error handling

--- test/xor-stdio.test.js
+++
@@ -1,8 +0,0 @@
-var endpoints = require('./duplexer').createCrossover()
-  , XorWrapper = require('../lib/buffered/xor');
-
-var encoder = new XorWrapper(endpoints[0]);
-var decoder = new XorWrapper(endpoints[1]);
-
-decoder.pipe(process.stdout);
-process.stdin.pipe(encoder);
